8e971a4460a70a10aae9c2f3e24a83a991e31603
David <dlarsen13@my.wctc.edu>
2023-02-25 13:35:49 -0600

    Initial commit with StreamReader for data intake

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string dir = Directory.GetCurrentDirectory();
+try {
+StreamReader sr = new StreamReader(dir + "\\ml-latest-small\\movies.csv");
+Console.WriteLine("File found");
+}
+catch{
+    Console.WriteLine("File not found");
+}
+
